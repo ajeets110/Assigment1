e95063b601cd0987eddc4b004a658f733e5c00aa
Ajeet Singh <ajeets110@gmail.com>
2020-01-14 16:30:53 -0500

    init

--- C0763441_FindMyWay/ViewController.swift
+++ C0763441_FindMyWay/ViewController.swift
@@ -7,14 +7,63 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    var locationManager = CLLocationManager()
+ 
+    @IBOutlet weak var navigateButton: UIButton!
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+                
+        let userLocation: CLLocation = locations[0]
+        
+        // getting latitude and longitude
+        let lat = userLocation.coordinate.latitude
+        let long = userLocation.coordinate.longitude
+        
+        // getting deltas for span
+        let latDelta : CLLocationDegrees = 0.05
+        let longDelta : CLLocationDegrees = 0.05
+               
+        // setting span and location
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        let location = CLLocationCoordinate2D(latitude: lat, longitude: long)
+               
+        // setting region
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        // add double tap gesture
+        let uidtgr = UITapGestureRecognizer(target: self, action: #selector(doubuleTap))
+        uidtgr.numberOfTapsRequired = 2
+        mapView.addGestureRecognizer(uidtgr)
+        
+    }
+    
+    @objc func doubuleTap(gestureRecognizer: UIGestureRecognizer){
+        
+        let touchPoint = gestureRecognizer.location(in: mapView)
+        let coordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = "Destination"
+        annotation.coordinate = coordinate
+        mapView.addAnnotation(annotation)
+    }
 
 }
 
